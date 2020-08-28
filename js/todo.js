11732ca92c18b05f94d6546cec300236db0428a9
Maxim Koltovich <12345kolt@gmail.com>
2020-08-28 16:58:36 +0300

    работа над добовлением задач в список продолжается,задачи добовляются но не исчезают из строки ввода.Добавил checkbox.

--- js/todo.js
+++ js/todo.js
@@ -1,10 +1,13 @@
 // TODO: код писать ниже --->
 
 // Создайте новый элемент списка при нажатии на кнопку "Добавить"
-window.onload = function() {
+window.onload = function () {
 
     let todoList = [];
-
+    if (localStorage.getItem('todo') != undefined) {
+        todoList = JSON.parse(localStorage.getItem('todo'));
+        out();
+    }
     document.getElementById("addButton").onclick = function () {
         let add = document.getElementById("itemInput").value;
         //{todo : Добавить хлеб, check: false}
@@ -15,13 +18,20 @@ window.onload = function() {
         todoList[i] = temp;
         console.log(todoList);
         out();
+        localStorage.setItem('todo', JSON.stringify(todoList));
 
     }
 
     function out() {
         let out = '';
         for (var key in todoList) {
+            if (todoList[key].check == true) {
+                out += '<input type="checkbox" checked>';
+            } else {
+                out += '<input type="checkbox">'
+            }
             out += todoList[key].todo + '<br>';
+
         }
         document.getElementById('todoList').innerHTML = out;
 
