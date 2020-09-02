ed87e66492f22fb45340897c4a71e7201347ac1c
Maxim Koltovich <12345kolt@gmail.com>
2020-09-02 20:13:25 +0300

    работаю над удалением задач из списка продолжаю

--- js/todo.js
+++ js/todo.js
@@ -41,47 +41,15 @@ window.onload = function () {
 
 //удалять элемент из списка при нажатии на кнопку "Очистить список"
 let clearButton = document.createElement("button").onclick;
+//const list = document.getElementById('todoList')
 
-const classNames = {
-    TODO_ITEM: 'todo-container',
-    TODO_CHECKBOX: 'todo-checkbox',
-    TODO_TEXT: 'todo-text',
-    TODO_DELETE: 'todo-delete',
-}
 
-const list = document.getElementById('todo-list')
-const itemCountSpan = document.getElementById('item-count')
-const uncheckedCountSpan = document.getElementById('unchecked-count')
 
-//This function creates a new list element
-function newTodo() {
-    var clicks = document.getElementsByTagName("input").length;
-    var item = prompt("Please enter a goal", " ")
-    if(item == null){
-        return cheese
-    }
-    clicks ++;
-    itemCountSpan.innerHTML = clicks;
-    list.innerHTML = list.innerHTML + "<li id = 'clicks'> <input type = checkbox onclick = 'checker()' class = 'todo-checkbox'>Goal: " + item +
-        "</input>" + "<button class = 'todo-delete' value = clicks onclick = 'deleteToDo(this.value)'>Delete</button></li>"
+function deliteButton(){
 
-}
-//This function sets the unchecked count
-function checker(){
-    var Total = 0;
-    for (var i = 0; i < document.getElementsByTagName("input").length; i++){
-        if (!document.getElementsByTagName("input")[i].checked){
-            Total ++;
-            uncheckedCountSpan.innerHTML = Total;
-        }
-    }
-}
-//This function deletes a list element selected
-function deleteToDo(sel_id){
-
-    list.removeChild(document.getElementsByTagName("li")[sel_id]);
-    var clicks = document.getElementsByTagName("input").length;
-    itemCountSpan.innerHTML = clicks;
+    list.removeChild(document.getElementsByTagName("ul"));
+    var clicks = document.getElementsByTagName("todoList").length;
+    clearButton.innerHTML = clicks;
 
 }
 
